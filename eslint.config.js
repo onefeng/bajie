e810336af8cd573629378dd1ec22ec70a113857f
onefeng <guosong@onefeng.xyz>
2026-04-15 15:39:40 +0800

    fix: 1

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfigWithVueTs(
